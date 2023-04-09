6f337588c690dd7b5a19fa5b140232df74919fb6
pratikderepatil <pratikdere333@gmail.com>
2023-04-09 21:57:46 +0530

    Frontend: Login done | UserForm done | Light Dark Mode done

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,21 @@
+import { Flex, Text } from '@chakra-ui/react';
 import React from 'react';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Flex
+      justifyContent={'space-between'}
+      p={5}
+      pl={['5', '10']}
+      pr={['5', '10']}
+    >
+      <Text fontFamily={'cursive'} fontWeight={'bold'}>
+        Social Us
+      </Text>
+      <ColorModeSwitcher justifySelf="flex-end" />
+    </Flex>
+  );
 };
 
 export default Navbar;
--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import HomePage from '../pages/HomePage';
+import UserForm from '../components/UserForm';
+import Login from '../components/Login';
 
 const AllRoutes = () => {
-  return <div>AllRoutes</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<UserForm />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="" />
+    </Routes>
+  );
 };
 
 export default AllRoutes;
